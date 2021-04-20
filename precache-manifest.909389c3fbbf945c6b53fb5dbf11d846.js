6d64d5871b7026080a46955006cc63151cc72ca6
Gustavo Monteiro <gustavo.monteiro@rhodiumhealthtec.com>
2021-04-20 17:29:17 +0100

    Updates

---
+++ precache-manifest.909389c3fbbf945c6b53fb5dbf11d846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e5593fe6f7b7ad116eb9fd57ea248f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "60281614022497045c2a",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "f8b25fa5ecc0a1ad1baa",
+    "url": "./static/js/2.0937ff0d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0937ff0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60281614022497045c2a",
+    "url": "./static/js/main.9cb953d5.chunk.js"
+  },
+  {
+    "revision": "09a3c02f507bf136a98d",
+    "url": "./static/js/runtime-main.abb9889d.js"
+  }
+]);
